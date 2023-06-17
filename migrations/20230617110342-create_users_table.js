e195e08867b4f5f721169c528bba94eb42702bfb
Mahmoud Elshafaey <mahmoud.elshafaey@linkdev.com>
2023-06-17 14:30:48 +0300

    make sequelize use env variables

--- migrations/20230617110342-create_users_table.js
+++ migrations/20230617110342-create_users_table.js
@@ -16,6 +16,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
       }
     });
   },
--- seeders/20230617110802-demo-users.js
+++ seeders/20230617110802-demo-users.js
@@ -6,9 +6,11 @@ module.exports = {
 
     // Generate 100 user records
     for (let i = 0; i < 100; i++) {
+      //create 100 user with password 12345678
       usersData.push({
         name: `User ${i + 1}`,
         email: `user${i + 1}@example.com`,
+        password: "$2b$10$txTUun4VigCDTUEqFRdvBOGUfOEnAnfbyBOEX9Kj/BX9U9Q9rEBT2",
       });
     }
 
